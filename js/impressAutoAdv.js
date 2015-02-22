27b7804730aa67881f419d3e4f9ddf64f3953f8b
Thomas Liske <thomas@fiasko-nw.net>
2015-02-23 00:25:10 +0100

    Drop debugging stuff.

--- js/impressAutoAdv.js
+++ js/impressAutoAdv.js
@@ -1,14 +1,13 @@
 (function ( document, window ) {
     'use strict';
-    
+
     document.addEventListener("impress:stepenter", function (event) {
 	updateProgressbar(event);
     });
 
     function updateProgressbar(e) {
-	console.log(e.target.id);
-	console.log(e);
 	var t = e.target.dataset["autoadv"] * 1000;
+
 	if(t) {
 	    window.setTimeout(function() {
 		window.impress().next();
